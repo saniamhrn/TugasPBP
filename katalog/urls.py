a20b1c6b9b7c38494d9dd180dd08a968f7abea21
saniarizqi <sania.rizqi@ui.ac.id>
2022-09-14 19:14:39 +0700

    progress tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,11 @@
 # TODO: Implement Routings Here
+
+from urllib.parse import urlparse
+from django.urls import path
+from katalog.views import show_katalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+        path('', show_katalog, name='show_katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+        data_barang_katalog = CatalogItem.objects.all()
+        context = {
+                'list_katalog': data_barang_katalog,
+                'nama': 'Sania Rizqi Maharani',
+                'student_id': '2006597001'
+        }
+        return render(request, "katalog.html", context)
